f933cdaff70ccfddeabbc03b0eba5ad075fed1c0
snds_prfct <snds.prfct@gmail.com>
2025-09-27 10:18:05 +0300

    'outbox_events' table and entity

---
+++ orders-service/src/main/java/dev/sndsprfct/orders/entity/outbox/OutboxEvent.java
@@ -0,0 +1,64 @@
+package dev.sndsprfct.orders.entity.outbox;
+
+import dev.sndsprfct.orders.constant.OutboxEventStatus;
+import dev.sndsprfct.orders.constant.OutboxEventType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(schema = "outbox", name = "outbox_events")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Setter
+@Getter
+public class OutboxEvent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "created_at")
+    private Instant createdAt = Instant.now();
+
+    @Column(name = "type")
+    @Enumerated(EnumType.STRING)
+    private OutboxEventType type;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private OutboxEventStatus status = OutboxEventStatus.PENDING;
+
+    @Column(name = "payload")
+    private String payload;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OutboxEvent that = (OutboxEvent) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+}
---
+++ orders-service/src/main/resources/db/migration/V2025.09.26_1__outbox_events_table.sql
@@ -0,0 +1,11 @@
+
+CREATE SCHEMA outbox;
+
+CREATE TABLE outbox.outbox_events (
+    id bigserial PRIMARY KEY,
+    created_at timestamp NOT NULL DEFAULT NOW(),
+    type varchar NOT NULL CHECK (type in ('ORDER_CREATED')),
+    status varchar NOT NULL CHECK (status in ('PENDING', 'SENT')) DEFAULT 'PENDING',
+    update_at timestamp,
+    payload jsonb NOT NULL
+);

488695d8154b36b7d1ae8d75984644c9c4583b9c
snds_prfct <snds.prfct@gmail.com>
2025-09-27 11:35:25 +0300

    Import refactoring

--- orders-service/src/main/java/dev/sndsprfct/orders/dto/ErrorDetails.java
+++ orders-service/src/main/java/dev/sndsprfct/orders/dto/ErrorDetails.java
@@ -1,8 +1,5 @@
 package dev.sndsprfct.orders.dto;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-
 import java.time.Instant;
 
 public record ErrorDetails(Instant timestamp, int status, String error) {
